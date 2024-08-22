97a5c665aa60534941e6e9c306658a6454d0b934
Dmitry Ilyevsky <ilyevsky@gmail.com>
2024-08-22 12:06:55 -0700

    [cmd/backplane] Auto-privision Proxy via --proxy_path flag

--- cmd/backplane/main.go
+++ cmd/backplane/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
@@ -16,19 +17,24 @@ import (
 	chdriver "github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/rest"
 	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
+	"github.com/apoxy-dev/apoxy-cli/client/versioned"
 	"github.com/apoxy-dev/apoxy-cli/internal/apiserver"
 	bpctrl "github.com/apoxy-dev/apoxy-cli/internal/backplane/controllers"
 	"github.com/apoxy-dev/apoxy-cli/internal/backplane/kvstore"
 	"github.com/apoxy-dev/apoxy-cli/internal/backplane/wasm/ext_proc"
 	"github.com/apoxy-dev/apoxy-cli/internal/backplane/wasm/manifest"
+	"github.com/apoxy-dev/apoxy-cli/internal/cmd/utils"
 	"github.com/apoxy-dev/apoxy-cli/internal/log"
 
 	ctrlv1alpha1 "github.com/apoxy-dev/apoxy-cli/api/controllers/v1alpha1"
@@ -43,9 +49,11 @@ func init() {
 }
 
 var (
-	projectID   = flag.String("project_id", "", "Apoxy project UUID.")
-	proxyName   = flag.String("proxy", "", "Name of the proxy to manage.")
-	replicaName = flag.String("replica", "", "Name of the replica to manage.")
+	projectID = flag.String("project_id", "", "Apoxy project UUID.")
+
+	proxyPath   = flag.String("proxy_path", "", "Path to the Proxy to create in the API.")
+	proxyName   = flag.String("proxy", "", "Name of the Proxy to manage. Must not be used with --proxy_path.")
+	replicaName = flag.String("replica", os.Getenv("HOSTNAME"), "Name of the replica to manage.")
 
 	devMode = flag.Bool("dev", false, "Enable development mode.")
 
@@ -65,26 +73,86 @@ var (
 	kvPeerSelector = flag.String("kv_peer_selector", "app=apoxy-backplane", "Label selector for K/V store peer.")
 )
 
+func upsertProxyFromPath(ctx context.Context, rC *rest.Config, path string) (string, error) {
+	proxyConfig, err := os.ReadFile(path)
+	if err != nil {
+		return "", fmt.Errorf("failed to read file: %w", err)
+	}
+	proxy := &ctrlv1alpha1.Proxy{}
+	proxyJSON, err := utils.YAMLToJSON(string(proxyConfig))
+	if err != nil {
+		// Try assuming that the config is a JSON string?
+		proxyJSON = string(proxyConfig)
+	}
+	err = json.Unmarshal([]byte(proxyJSON), proxy)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal Proxy config: %w", err)
+	}
+
+	c := versioned.NewForConfigOrDie(rC)
+	_, err = c.ControllersV1alpha1().Proxies().Create(ctx, proxy, metav1.CreateOptions{})
+	if errors.IsAlreadyExists(err) {
+		e, err := c.ControllersV1alpha1().Proxies().Get(ctx, proxy.Name, metav1.GetOptions{})
+		if err != nil {
+			return "", fmt.Errorf("failed to get existing Proxy: %w", err)
+		}
+
+		proxy.ResourceVersion = e.ResourceVersion
+
+		_, err = c.ControllersV1alpha1().Proxies().Update(ctx, proxy, metav1.UpdateOptions{})
+		if err != nil {
+			return "", fmt.Errorf("failed to update existing Proxy: %w", err)
+		}
+
+		log.Infof("Proxy %s updated", proxy.Name)
+
+		return proxy.Name, nil
+	} else if err != nil {
+		return "", fmt.Errorf("failed to create Proxy: %w", err)
+	}
+
+	log.Infof("Proxy %s created", proxy.Name)
+
+	return proxy.Name, nil
+}
+
 func main() {
 	flag.Parse()
 	var lOpts []log.Option
 	if *devMode {
-		lOpts = append(lOpts, log.WithDevMode())
+		lOpts = append(lOpts, log.WithDevMode(), log.WithAlsoLogToStderr())
 	}
 	log.Init(lOpts...)
-
-	projUUID, err := uuid.Parse(*projectID)
-	if err != nil {
-		log.Fatalf("invalid project UUID: %v", err)
-	}
-	if *proxyName == "" {
-		log.Fatalf("proxy name is required")
-	}
-
 	ctx := ctrl.SetupSignalHandler()
+
+	if *apiserverHost == "" {
+		log.Fatalf("--apiserver_host must be set")
+	}
+	rC := apiserver.NewClientConfig(apiserver.WithClientHost(*apiserverHost))
+
+	if *proxyPath == "" && *proxyName == "" {
+		log.Fatalf("either --proxy_path or --proxy must be set")
+	}
+	if *proxyPath != "" {
+		var err error
+		*proxyName, err = upsertProxyFromPath(ctx, rC, *proxyPath)
+		if err != nil {
+			log.Fatalf("Failed to update proxy from path: %v", err)
+		}
+	} else if *proxyName != "" {
+		if *replicaName == "" {
+			log.Fatalf("--replica must be set when --proxy is set")
+		}
+	} else {
+		log.Fatalf("only one of --proxy_path or --proxy must be set")
+	}
 
 	var chConn chdriver.Conn
 	if *chAddrs != "" {
+		projUUID, err := uuid.Parse(*projectID)
+		if err != nil {
+			log.Fatalf("invalid project UUID: %v", err)
+		}
 		log.Infof("Connecting to ClickHouse at %v", *chAddrs)
 		chOpts := &clickhouse.Options{
 			Addr: strings.Split(*chAddrs, ","),
@@ -103,7 +171,6 @@ func main() {
 			chOpts.TLS = &tls.Config{}
 		}
 		// TODO(dsky): Wrap this for lazy initialization to avoid blocking startup.
-		var err error
 		chConn, err = clickhouse.Open(chOpts)
 		if err != nil {
 			log.Fatalf("Failed to connect to ClickHouse: %v", err)
@@ -154,7 +221,6 @@ func main() {
 
 	log.Infof("Setting up managers")
 
-	rC := apiserver.NewClientConfig(apiserver.WithClientHost(*apiserverHost))
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true))) // TODO(dilyevsky): Use default golang logger.
 	mgr, err := ctrl.NewManager(rC, ctrl.Options{
 		Cache: cache.Options{
