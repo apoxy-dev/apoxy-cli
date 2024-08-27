6344c86c49c2538ca9c7cc3ec58acf83d319090f
Dmitry Ilyevsky <ilyevsky@gmail.com>
2024-08-27 22:16:13 +0400

    [backplane/dns] Actually resolve CNAME queries

--- internal/backplane/dns/dns.go
+++ internal/backplane/dns/dns.go
@@ -61,53 +61,117 @@ func (r *Resolver) Start(
 }
 
 func (r *Resolver) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
-	key := req.Question[0].Name
+	domain := req.Question[0].Name
 	dm, err := r.store.CNAMEMap()
 	if err != nil {
 		log.Errorf("failed to get CNAME map: %v", err)
-		_ = w.WriteMsg(reply(req, key, "", dns.RcodeServerFailure, 0))
+		_ = w.WriteMsg(replyError(req, dns.RcodeServerFailure))
 		return
 	}
-	dmr, err := dm.Get(r.ctx, key)
+	dmr, err := dm.Get(r.ctx, domain)
 	if err == olric.ErrKeyNotFound {
-		log.Debugf("Domain not found in DM. Resolving %s with upstream %s", key, r.upstream)
+		log.Debugf("Domain not found in DM. Resolving %s with upstream %s", domain, r.upstream)
 
-		fwd := new(dns.Client)
-		fwd.Dialer = &net.Dialer{
+		up := new(dns.Client)
+		up.Dialer = &net.Dialer{
 			Timeout: 2 * time.Second,
 		}
-		up, _, err := fwd.Exchange(req, r.upstream+":53")
+		rr, _, err := up.Exchange(req, r.upstream+":53")
 		if err != nil {
-			log.Errorf("failed to resolve %s with upstream %s: %v", key, r.upstream, err)
-			_ = w.WriteMsg(reply(req, key, "", dns.RcodeServerFailure, 0))
+			log.Errorf("failed to resolve %s with upstream %s: %v", domain, r.upstream, err)
+			_ = w.WriteMsg(replyError(req, dns.RcodeServerFailure))
 			return
 		}
 
-		_ = w.WriteMsg(up)
+		_ = w.WriteMsg(rr)
 		return
 	} else if err != nil {
-		log.Errorf("failed to get CNAME record for %s: %v", key, err)
-		_ = w.WriteMsg(reply(req, key, "", dns.RcodeNameError, 0))
+		log.Errorf("failed to get CNAME record for %s: %v", domain, err)
+		_ = w.WriteMsg(replyError(req, dns.RcodeNameError))
 		return
 	}
-	cname, err := dmr.String()
+	val, err := dmr.String()
 	if err != nil {
-		log.Errorf("failed to parse CNAME record for %s: %v", key, err)
-		_ = w.WriteMsg(reply(req, key, "", dns.RcodeServerFailure, 0))
+		log.Errorf("failed to parse CNAME record for %s: %v", domain, err)
+		_ = w.WriteMsg(replyError(req, dns.RcodeServerFailure))
 		return
 	}
 
-	log.Debugf("Resolved %s to %s", key, cname)
+	ttl := int(time.Duration(dmr.TTL()) / time.Second)
+	if ttl == 0 {
+		ttl = 60
+	}
 
-	_ = w.WriteMsg(reply(req, key, cname, dns.RcodeSuccess, int(time.Duration(dmr.TTL())/time.Second)))
+	_, ok := dns.IsDomainName(val)
+	if !ok {
+		rrVal := net.ParseIP(val)
+		if rrVal == nil {
+			log.Errorf("invalid RR value (not IP or domain): %s", val)
+			_ = w.WriteMsg(replyError(req, dns.RcodeServerFailure))
+			return
+		}
+		// val is an IP address
+		log.Debugf("Resolved %s to A %s", domain, rrVal)
+
+		_ = w.WriteMsg(replyA(req, domain, rrVal, ttl))
+		return
+	}
+
+	// val is a domain name - CNAME resolution (only one level).
+	log.Debugf("Resolved %s to CNAME %s", domain, val)
+	up := new(dns.Client)
+	up.Dialer = &net.Dialer{
+		Timeout: 2 * time.Second,
+	}
+	areq := new(dns.Msg)
+	areq.SetQuestion(val, dns.TypeA)
+	rr, _, err := up.Exchange(areq, r.upstream+":53")
+	if err != nil {
+		log.Errorf("failed to resolve %s with upstream %s: %v", domain, r.upstream, err)
+		_ = w.WriteMsg(replyError(req, dns.RcodeServerFailure))
+		return
+	}
+	var vals []net.IP
+	for _, a := range rr.Answer {
+		if a.Header().Rrtype == dns.TypeA {
+			log.Debugf("Resolved CNAME %s to A %s", val, a.(*dns.A).A)
+			vals = append(vals, a.(*dns.A).A)
+			break
+		}
+	}
+	if len(vals) == 0 {
+		log.Errorf("failed to resolve CNAME %s to A record", val)
+		_ = w.WriteMsg(replyError(req, dns.RcodeServerFailure))
+		return
+	}
+
+	_ = w.WriteMsg(replyCNAME(req, domain, val, vals, ttl))
 }
 
-func reply(req *dns.Msg, domain, cname string, rcode, ttl int) *dns.Msg {
+func replyError(req *dns.Msg, rcode int) *dns.Msg {
 	resp := &dns.Msg{}
 	resp.SetRcode(req, rcode)
-	if rcode != dns.RcodeSuccess {
-		return resp
-	}
+	return resp
+}
+
+func replyA(req *dns.Msg, domain string, val net.IP, ttl int) *dns.Msg {
+	resp := &dns.Msg{}
+	resp.SetReply(req)
+	resp.Answer = append(resp.Answer, &dns.A{
+		Hdr: dns.RR_Header{
+			Name:   domain,
+			Rrtype: dns.TypeA,
+			Class:  dns.ClassINET,
+			Ttl:    uint32(ttl),
+		},
+		A: val,
+	})
+	return resp
+}
+
+func replyCNAME(req *dns.Msg, domain, cname string, vals []net.IP, ttl int) *dns.Msg {
+	resp := &dns.Msg{}
+	resp.SetReply(req)
 	resp.Answer = append(resp.Answer, &dns.CNAME{
 		Hdr: dns.RR_Header{
 			Name:   domain,
@@ -117,5 +181,16 @@ func reply(req *dns.Msg, domain, cname string, rcode, ttl int) *dns.Msg {
 		},
 		Target: cname,
 	})
+	for _, val := range vals {
+		resp.Answer = append(resp.Answer, &dns.A{
+			Hdr: dns.RR_Header{
+				Name:   cname,
+				Rrtype: dns.TypeA,
+				Class:  dns.ClassINET,
+				Ttl:    uint32(ttl),
+			},
+			A: val,
+		})
+	}
 	return resp
 }
