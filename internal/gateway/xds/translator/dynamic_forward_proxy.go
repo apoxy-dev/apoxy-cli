3ec19100015c3b3a1c72095f1cbaee06332aec2a
Dmitry Ilyevsky <ilyevsky@gmail.com>
2024-10-13 11:26:16 -0700

    [gateway] Disable our custom resolver

--- internal/gateway/xds/translator/dynamic_forward_proxy.go
+++ internal/gateway/xds/translator/dynamic_forward_proxy.go
@@ -6,13 +6,11 @@ import (
 	"strings"
 
 	clusterv3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
-	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	routev3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
 	dfpclusterv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/clusters/dynamic_forward_proxy/v3"
 	dfpconfigv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/common/dynamic_forward_proxy/v3"
 	dfpfilterv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/dynamic_forward_proxy/v3"
 	hcmv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
-	caresv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/network/dns_resolver/cares/v3"
 	resourcev3 "github.com/envoyproxy/go-control-plane/pkg/resource/v3"
 	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/durationpb"
@@ -20,7 +18,6 @@ import (
 
 	"github.com/apoxy-dev/apoxy-cli/internal/gateway/ir"
 	"github.com/apoxy-dev/apoxy-cli/internal/gateway/xds/types"
-	"github.com/apoxy-dev/apoxy-cli/internal/log"
 )
 
 const (
@@ -115,26 +112,26 @@ func dnsCacheConfig(dfp *ir.DynamicForwardProxy) *dfpconfigv3.DnsCacheConfig {
 		maxHosts = wrapperspb.UInt32(*dfp.MaxHosts)
 	}
 
-	caresConfig, err := anypb.New(&caresv3.CaresDnsResolverConfig{
-		Resolvers: []*corev3.Address{
-			{
-				// Point to the local DNS server embedded within Backplane.
-				Address: &corev3.Address_SocketAddress{
-					SocketAddress: &corev3.SocketAddress{
-						Protocol: corev3.SocketAddress_UDP,
-						Address:  "127.0.0.1",
-						PortSpecifier: &corev3.SocketAddress_PortValue{
-							PortValue: 1053,
-						},
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		log.Errorf("failed to marshal cares dns resolver config: %v", err)
-		return nil
-	}
+	//caresConfig, err := anypb.New(&caresv3.CaresDnsResolverConfig{
+	//	Resolvers: []*corev3.Address{
+	//		{
+	//			// Point to the local DNS server embedded within Backplane.
+	//			Address: &corev3.Address_SocketAddress{
+	//				SocketAddress: &corev3.SocketAddress{
+	//					Protocol: corev3.SocketAddress_UDP,
+	//					Address:  "127.0.0.1",
+	//					PortSpecifier: &corev3.SocketAddress_PortValue{
+	//						PortValue: 1053,
+	//					},
+	//				},
+	//			},
+	//		},
+	//	},
+	//})
+	//if err != nil {
+	//	log.Errorf("failed to marshal cares dns resolver config: %v", err)
+	//	return nil
+	//}
 
 	return &dfpconfigv3.DnsCacheConfig{
 		Name:            dfp.Name,
@@ -142,10 +139,10 @@ func dnsCacheConfig(dfp *ir.DynamicForwardProxy) *dfpconfigv3.DnsCacheConfig {
 		DnsRefreshRate:  dnsRefreshRate,
 		HostTtl:         hostTTL,
 		MaxHosts:        maxHosts,
-		TypedDnsResolverConfig: &corev3.TypedExtensionConfig{
-			Name:        "envoy.extensions.network.dns_resolver.cares",
-			TypedConfig: caresConfig,
-		},
+		//TypedDnsResolverConfig: &corev3.TypedExtensionConfig{
+		//	Name: "envoy.extensions.network.dns_resolver.cares",
+		//	TypedConfig: caresConfig,
+		//},
 	}
 }
 
