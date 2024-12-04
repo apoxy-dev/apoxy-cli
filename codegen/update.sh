f6d49f4c0a3fce58c36bf04cd19945f0b8083c6b
Dmitry Ilyevsky <ilyevsky@gmail.com>
2024-12-03 23:09:31 -0800

    Add k8s.io/apimachinery/pkg/runtime to register-gen explicitly

--- codegen/update.sh
+++ codegen/update.sh
@@ -11,55 +11,6 @@ export GOTOOLCHAIN=go1.23.3 # Should match the go version in go.mod
 CODEGEN_VERSION=v0.30.1 # Should match the k8s.io/apimachinery version in go.mod
 BOILERPLATE_FILE="${ROOT_DIR}/codegen/boilerplate.go.txt"
 
-# For some reason modern versions of register-gen don't include imports for 
-# the runtime and schema packages.
-function fixup_register_gen_imports() {
-  local root_dir=$1
-  local imports=(
-    '"k8s.io/apimachinery/pkg/runtime"'
-    '"k8s.io/apimachinery/pkg/runtime/schema"'
-  )
-
-  find "$root_dir" -type f -name "zz_generated.register.go" | while read -r file; do
-    # Check if all imports already exist in the file
-    local missing_imports=()
-    for import in "${imports[@]}"; do
-      if ! grep -q "$import" "$file"; then
-        missing_imports+=("$import")
-      fi
-    done
-
-    # If no imports are missing, skip the file
-    if [ ${#missing_imports[@]} -eq 0 ]; then
-      continue
-    fi
-
-    # Process the file to add missing imports
-    awk -v missing_imports="$(printf "%s\n" "${missing_imports[@]}")" '
-      BEGIN {
-        split(missing_imports, imports, "\n");
-        in_imports = 0;
-      }
-      /import \(/ && !in_imports {
-        in_imports = 1;
-        print;
-        for (i in imports) {
-          print "\t" imports[i];
-        }
-        next;
-      }
-      in_imports && /\)/ {
-        in_imports = 0;
-        print;
-        next;
-      }
-      {
-        print;
-      }
-    ' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
-  done
-}
-
 function generate_helpers() {
   local dirs=("$@")
   for dir in "${dirs[@]}"; do
@@ -73,9 +24,9 @@ function generate_helpers() {
     go run "k8s.io/code-generator/cmd/register-gen@${CODEGEN_VERSION}" \
       --output-file zz_generated.register.go \
       --go-header-file "${BOILERPLATE_FILE}" \
+      k8s.io/apimachinery/pkg/runtime \
       "$dir"
 
-    fixup_register_gen_imports "$dir"
   done
 }
 
