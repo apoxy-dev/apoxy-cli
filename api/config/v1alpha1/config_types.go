a80d068ee29bd5444c78cc0b15f541db06f84e95
Damian Peckett <damian@pecke.tt>
2024-12-09 13:30:46 +0100

    refactor: reusing client-go primitives wasn't worth it, and wire everything up

--- api/config/v1alpha1/config_types.go
+++ api/config/v1alpha1/config_types.go
@@ -5,7 +5,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	clientcmdv1 "k8s.io/client-go/tools/clientcmd/api/v1"
 )
 
 var _ runtime.Object = (*Config)(nil)
@@ -19,58 +18,33 @@ var _ runtime.Object = (*Config)(nil)
 // Config is the Schema for the Apoxy Backplane configuration.
 type Config struct {
 	metav1.TypeMeta `json:",inline"`
-	// The project ID to use for authentication.
-	ProjectID uuid.UUID `json:"projectID,omitempty"`
 	// Whether to enable verbose logging.
 	Verbose bool `json:"verbose,omitempty"`
 	// The URL for the dashboard UI.
 	DashboardURL string `json:"dashboardURL,omitempty"`
-	// Servers is a map of referencable names to API server configs.
-	Servers []NamedServer `json:"servers"`
-	// Users is a map of referencable names to user configs.
-	Users []clientcmdv1.NamedAuthInfo `json:"users"`
-	// Contexts is a map of referencable names to context configs.
-	Contexts []NamedContext `json:"contexts"`
+	// CurrentProject is the default project ID to use unless overridden.
+	CurrentProject uuid.UUID `json:"currentProject,omitempty"`
+	// Projects is a list of projects that this instance is managing.
+	Projects []Project `json:"projects,omitempty"`
 	// Resources is a list of resources that this instance should manage/reconcile.
-	Resources []ObjectReferenceWithContext `json:"resources"`
+	Resources []ObjectReferenceWithProject `json:"resources,omitempty"`
 }
 
-// NamedServer relates nicknames to API server information.
-type NamedServer struct {
-	// Name is the nickname for this server.
-	Name string `json:"name"`
-	// Server is the API server information.
-	Server Server `json:"server"`
-}
-
-// Server is an API server.
-type Server struct {
-	clientcmdv1.Cluster `json:",inline"`
-	// The host header to set for requests.
-	Host string `yaml:"host,omitempty"`
-}
-
-// NamedContext relates nicknames to context information.
-type NamedContext struct {
-	// Name is the nickname for this context.
-	Name string `json:"name"`
-	// Context is the context information.
-	Context Context `json:"context"`
-}
-
-// Context is a context for an API server.
-type Context struct {
-	// Server is the name of the server for this context.
-	Server string `json:"server"`
-	// User is the name of the authInfo for this context.
-	User string `json:"user"`
-	// Namespace is the default namespace to use on unspecified requests.
-	Namespace string `json:"namespace,omitempty"`
+// Project is a configuration for a project.
+type Project struct {
+	// ID is the project ID.
+	ID uuid.UUID `json:"id"`
+	// The base URL for API requests.
+	APIBaseURL string `json:"apiBaseURL,omitempty"`
+	// The host header to set for API requests.
+	APIBaseHost string `json:"apiBaseHost,omitempty"`
+	// APIKey is the API key for the project.
+	APIKey string `json:"apiKey"`
 }
 
 // ObjectReferenceWithContext is a reference to an object with an API server context.
-type ObjectReferenceWithContext struct {
+type ObjectReferenceWithProject struct {
 	corev1.ObjectReference `json:",inline"`
-	// Context is the context to use for the resource.
-	Context string `json:"context"`
+	// Project is the project ID to use for this object.
+	Project string `json:"project"`
 }
