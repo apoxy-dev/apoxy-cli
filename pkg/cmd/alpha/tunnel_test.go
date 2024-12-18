d5716c26c564171f49952699273987e15ab3868e
Damian Peckett <damian@pecke.tt>
2024-12-18 20:15:24 +0100

    [wg] miscellanous tunnel reconciler improvements (#20)
    Plus add a local STUN server command for testing wireguard tunnels
    within a LAN.

--- pkg/cmd/alpha/tunnel_test.go
+++ pkg/cmd/alpha/tunnel_test.go
@@ -2,7 +2,6 @@ package alpha
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log/slog"
 	"net"
@@ -15,7 +14,6 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/pion/stun"
 	"github.com/stretchr/testify/require"
 	proxyclient "golang.org/x/net/proxy"
 	"golang.org/x/sync/errgroup"
@@ -63,7 +61,7 @@ func TestRunTunnel(t *testing.T) {
 
 	// Run a STUN server to handle STUN requests locally.
 	g.Go(func() error {
-		return listenForSTUNRequests(egCtx)
+		return listenForSTUNRequests(egCtx, "localhost:3478")
 	})
 
 	// Run two tunnel nodes
@@ -130,57 +128,6 @@ func TestRunTunnel(t *testing.T) {
 	})
 
 	require.Equal(t, http.StatusOK, resp.StatusCode)
-}
-
-// Run our own minimal STUN server to handle STUN requests as we want the local
-// port/address rather than the real external address.
-func listenForSTUNRequests(ctx context.Context) error {
-	pc, err := net.ListenPacket("udp", "localhost:3478")
-	if err != nil {
-		return fmt.Errorf("error setting up listener: %w", err)
-	}
-	defer pc.Close()
-
-	go func() {
-		<-ctx.Done()
-		if err := pc.Close(); err != nil {
-			slog.Error("Error closing listener", slog.Any("error", err))
-		}
-	}()
-
-	buf := make([]byte, 1024)
-	for {
-		n, addr, err := pc.ReadFrom(buf)
-		if err != nil && !errors.Is(err, net.ErrClosed) {
-			return fmt.Errorf("error reading from listener: %w", err)
-		}
-
-		message := new(stun.Message)
-		message.Raw = append([]byte{}, buf[:n]...)
-		if err := message.Decode(); err != nil {
-			return fmt.Errorf("error decoding STUN message: %w", err)
-		}
-
-		if err := handleSTUNMessage(message, addr, pc); err != nil {
-			return fmt.Errorf("error handling STUN message: %w", err)
-		}
-	}
-}
-
-func handleSTUNMessage(message *stun.Message, addr net.Addr, conn net.PacketConn) error {
-	if message.Type.Method == stun.MethodBinding && message.Type.Class == stun.ClassRequest {
-		response := stun.MustBuild(stun.TransactionID, stun.BindingSuccess,
-			stun.XORMappedAddress{IP: addr.(*net.UDPAddr).IP, Port: addr.(*net.UDPAddr).Port},
-			stun.MessageIntegrity([]byte("secret")),
-			stun.Fingerprint,
-		)
-
-		if _, err := conn.WriteTo(response.Raw, addr); err != nil {
-			return fmt.Errorf("error sending STUN response: %w", err)
-		}
-	}
-
-	return nil
 }
 
 func pollUntilReady(ctx context.Context, client versioned.Interface, name string) error {
