4382dc46d650cef5fab000f460a5daa0ef58e95a
Dmitry Ilyevsky <ilyevsky@gmail.com>
2025-01-31 23:53:19 -0800

    Fix kernel tunnel method signature on linux

--- pkg/tunnel/kernel_tunnel.go
+++ pkg/tunnel/kernel_tunnel.go
@@ -6,11 +6,14 @@ package tunnel
 import (
 	"context"
 	"errors"
+	"net/netip"
 )
 
 // CreateKernelTunnel creates a new kernel tunnel interface (WireGuard).
 func CreateKernelTunnel(
 	ctx context.Context,
+	addr netip.Prefix,
+	stunServers []string,
 ) (Tunnel, error) {
 	return nil, errors.New("kernel mode tunnel is not supported on this platform")
 }
