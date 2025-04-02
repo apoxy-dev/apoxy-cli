93a6c173c5732e29b51f4d63c3d3cae02d6b151c
Dmitry Ilyevsky <ilyevsky@gmail.com>
2025-04-01 17:52:12 -0700

    Fix test

--- pkg/tunnel/token/token_test.go
+++ pkg/tunnel/token/token_test.go
@@ -1,4 +1,4 @@
-package token_test
+package token
 
 import (
 	"crypto/ecdsa"
@@ -6,21 +6,17 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"encoding/pem"
-	"os"
-	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/stretchr/testify/require"
-
-	"apoxy.dev/masque-tunnel/internal/token"
 )
 
 func TestTokenValidator(t *testing.T) {
-	privateKey, publicKeyPath := generateKeyPair(t)
+	privateKey, publicKey := generateKeyPair(t)
 
-	validator, err := token.NewValidator(publicKeyPath)
+	validator, err := NewValidator(publicKey)
 	require.NoError(t, err)
 
 	t.Run("Valid", func(t *testing.T) {
@@ -59,7 +55,7 @@ func TestTokenValidator(t *testing.T) {
 	})
 }
 
-func generateKeyPair(t *testing.T) (*ecdsa.PrivateKey, string) {
+func generateKeyPair(t *testing.T) (*ecdsa.PrivateKey, []byte) {
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	require.NoError(t, err)
 
@@ -71,9 +67,5 @@ func generateKeyPair(t *testing.T) (*ecdsa.PrivateKey, string) {
 		Bytes: publicKeyBytes,
 	})
 
-	publicKeyPath := filepath.Join(t.TempDir(), "jwt.pem")
-	err = os.WriteFile(publicKeyPath, pemData, 0o600)
-	require.NoError(t, err)
-
-	return privateKey, publicKeyPath
+	return privateKey, pemData
 }
